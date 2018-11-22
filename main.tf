a23aad82d988386da4ea7e270e4b3de0233ef7a3
EC2 Default User <ec2-user@ip-172-31-29-214.ap-northeast-2.compute.internal>
2018-11-22 07:59:47 +0000

    initial commit

---
+++ main.tf
@@ -0,0 +1,7 @@
+# Download the ghost:latest docker_image "image_id" to the system using a Terraform resource
+resource "docker_image" "image_id" {
+  name = "ghost:latest"
+}
+resource "docker_image" "image_id1" {
+  name = "ubuntu:latest"
+}
